94ba9925db026130fd44e4dd85d914d34d8cf139
vegopunk <vegopunk@mail.ru>
2018-02-24 15:40:22 +0300

    ep.3 Pulsing Animations
    Создание анимации пульсирования. Создание цветовой гаммы приложения.

--- CircleProgressBar/ViewController.swift
+++ CircleProgressBar/ViewController.swift
@@ -12,54 +12,88 @@ class ViewController: UIViewController, URLSessionDownloadDelegate {
     
     
     
-    let shapeLayer = CAShapeLayer()
+    var shapeLayer : CAShapeLayer!
+    
+    var pulsatingLayer : CAShapeLayer!
     
     let percentageLabel : UILabel = {
        let label = UILabel()
         label.text = "Start"
         label.textAlignment = .center
         label.font = UIFont.boldSystemFont(ofSize: 32)
+        label.textColor = .white
         return label
     }()
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
 
+    
+    //возобнавляет анимацию пульсирования после сворачивания/разворачивания прлиожения
+    private func setupNotificationObservers() {
+        NotificationCenter.default.addObserver(self, selector: #selector(handleEnterForeground), name: .UIApplicationWillEnterForeground, object: nil)
+    }
+    
+    @objc private func handleEnterForeground() {
+        animatePulsatingLayer()
+    }
+    
+    private func createCircleShapeLayer(strokeColor: UIColor , fillColor : UIColor ) -> CAShapeLayer{
+        let layer = CAShapeLayer()
+        let circularPath = UIBezierPath(arcCenter: .zero, radius: 100, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
+        layer.path = circularPath.cgPath
+        layer.strokeColor = strokeColor.cgColor
+        layer.lineWidth = 20
+        layer.fillColor = fillColor.cgColor
+        layer.lineCap = kCALineCapRound
+        layer.position = view.center
+        return layer
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
+        setupNotificationObservers()
+        
+        view.backgroundColor = UIColor.backgroundColor
+        //create track layer
+        setupCircleLayers()
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
+        setupPercentageLabel()
+        
+        
+    }
+    
+    private func setupPercentageLabel() {
         view.addSubview(percentageLabel)
         percentageLabel.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
         percentageLabel.center = view.center
+    }
+    
+    private func setupCircleLayers() {
+        pulsatingLayer = createCircleShapeLayer(strokeColor: .clear, fillColor: UIColor.pulsatingFillColor)
+        view.layer.addSublayer(pulsatingLayer)
+        animatePulsatingLayer()
         
-//        let center = view.center
-        
-        //create track layer
-        
-        let trackLayer = CAShapeLayer()
-        
-        let circularPath = UIBezierPath(arcCenter: .zero, radius: 100, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
-        
-        trackLayer.path = circularPath.cgPath
-        trackLayer.strokeColor = UIColor.lightGray.cgColor
-        trackLayer.lineWidth = 10
-        trackLayer.fillColor = UIColor.clear.cgColor
-        trackLayer.lineCap = kCALineCapRound
-        trackLayer.position = view.center
+        let trackLayer = createCircleShapeLayer(strokeColor: .trackStrokeColor, fillColor: .backgroundColor)
         view.layer.addSublayer(trackLayer)
-        
-        shapeLayer.path = circularPath.cgPath
-        shapeLayer.strokeColor = UIColor.red.cgColor
-        shapeLayer.lineWidth = 10
-        shapeLayer.fillColor = UIColor.clear.cgColor
-        shapeLayer.lineCap = kCALineCapRound
-        shapeLayer.position = view.center
+        shapeLayer = createCircleShapeLayer(strokeColor: .outlinesStrokeColor, fillColor: .clear)
         
         shapeLayer.transform = CATransform3DMakeRotation(-CGFloat.pi / 2, 0, 0, 1)
         shapeLayer.strokeEnd = 0
         view.layer.addSublayer(shapeLayer)
-        
-        
-        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
-        
+    }
+    
+    private func animatePulsatingLayer() {
+    let animation = CABasicAnimation(keyPath: "transform.scale")
+        animation.toValue = 1.3
+        animation.duration = 0.8
+        animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)
+        animation.autoreverses = true
+        animation.repeatCount = Float.infinity
+        pulsatingLayer.add(animation, forKey: "pulsing")
     }
     
     let urlString = "https://firebasestorage.googleapis.com/v0/b/website-finally.appspot.com/o/source%2FEclipse%20Installer.app.zip?alt=media&token=66aaa984-58c7-4a41-bfdd-88cbf4215305"
